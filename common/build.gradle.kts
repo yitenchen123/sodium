e913eeac255772b5f9c9748c0003d2b06b88f4c9
yitenchen123 <2916782430@qq.com>
2026-06-07 17:22:37 +0800

    更新 build.gradle.kts

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -55,9 +55,9 @@ dependencies {
 
     // We need to be careful during pre-launch that we don't touch any Minecraft classes, since other mods
     // will not yet have an opportunity to apply transformations.
-    configurationPreLaunch("org.lwjgl:lwjgl:3.4.1")
-    configurationPreLaunch("org.lwjgl:lwjgl-opengl:3.4.1")
-    configurationPreLaunch("org.lwjgl:lwjgl-glfw:3.4.1")
+    configurationPreLaunch("org.lwjgl:lwjgl:3.3.3")
+    configurationPreLaunch("org.lwjgl:lwjgl-opengl:3.3.3")
+      configurationPreLaunch("org.lwjgl:lwjgl-glfw:3.3.3")
     configurationPreLaunch("net.java.dev.jna:jna:5.14.0")
     configurationPreLaunch("net.java.dev.jna:jna-platform:5.14.0")
     configurationPreLaunch("org.slf4j:slf4j-api:2.0.9")
