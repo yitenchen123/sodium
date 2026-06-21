ca52324c543ef0b5021d0310bc212967c1310286
yitenchen123 <2916782430@qq.com>
2026-06-21 10:56:16 +0800

    更新 PostLaunchChecks.java

--- common/src/boot/java/net/caffeinemc/mods/sodium/client/compatibility/checks/PostLaunchChecks.java
+++ common/src/boot/java/net/caffeinemc/mods/sodium/client/compatibility/checks/PostLaunchChecks.java
@@ -2,64 +2,12 @@ package net.caffeinemc.mods.sodium.client.compatibility.checks;
 
 import net.caffeinemc.mods.sodium.client.compatibility.environment.GlContextInfo;
 import net.caffeinemc.mods.sodium.client.compatibility.workarounds.nvidia.NvidiaWorkarounds;
-import net.caffeinemc.mods.sodium.client.console.Console;
-import net.caffeinemc.mods.sodium.client.console.message.MessageLevel;
 import net.caffeinemc.mods.sodium.client.platform.NativeWindowHandle;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-/**
- * Performs OpenGL driver validation after the game creates an OpenGL context. This runs immediately after OpenGL
- * context creation, and uses the implementation details of the OpenGL context to perform validation.
- */
 public class PostLaunchChecks {
-    private static final Logger LOGGER = LoggerFactory.getLogger("Sodium-PostlaunchChecks");
-
     public static void onContextInitialized(NativeWindowHandle window, GlContextInfo context) {
         GraphicsDriverChecks.postContextInit(window, context);
         NvidiaWorkarounds.applyContextChanges(context);
-
-        // ===== 修改开始：注释掉 PojavLauncher 检测 =====
-        // FIXME: This can be determined earlier, but we can't access the GUI classes in pre-launch
-        // if (isUsingPojavLauncher()) {
-        //     throw new RuntimeException("It appears that you are using PojavLauncher, which is not supported when " +
-        //             "using Sodium. Please check your mods list.");
-        // }
-        // ===== 修改结束 =====
-    }
-
-    // https://github.com/CaffeineMC/sodium/issues/1916
-    private static boolean isUsingPojavLauncher() {
-        if (System.getenv("POJAV_RENDERER") != null) {
-            LOGGER.warn("Detected presence of environment variable POJAV_LAUNCHER, which seems to indicate we are running on Android");
-
-            return true;
-        }
-
-        var librarySearchPaths = System.getProperty("java.library.path", null);
-
-        if (librarySearchPaths != null) {
-            for (var path : librarySearchPaths.split(":")) {
-                if (isKnownAndroidPathFragment(path)) {
-                    LOGGER.warn("Found a library search path which seems to be hosted in an Android filesystem: {}", path);
-
-                    return true;
-                }
-            }
-        }
-
-        var workingDirectory = System.getProperty("user.home", null);
-
-        if (workingDirectory != null) {
-            if (isKnownAndroidPathFragment(workingDirectory)) {
-                LOGGER.warn("Working directory seems to be hosted in an Android filesystem: {}", workingDirectory);
-            }
-        }
-
-        return false;
-    }
-
-    private static boolean isKnownAndroidPathFragment(String path) {
-        return path.matches("/data/user/[0-9]+/net\\.kdt\\.pojavlaunch");
+        // PojavLauncher 检测已完全移除，不再抛出异常
     }
 }
